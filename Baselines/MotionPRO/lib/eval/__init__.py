a8ff4d71d95bf071a9e320f53e0387f68b9a59c1
DavyFangyx <davyfangyuxuan@gmail.com>
2026-09-21 08:20:52 +0800

    整理文件

--- Baselines/MotionPRO/lib/eval/__init__.py
+++
@@ -1,8 +0,0 @@
-from lib.eval.metrics import METRIC_NAMES, compute_session_metrics, summarize_metrics, write_metrics_files
-
-__all__ = [
-    'METRIC_NAMES',
-    'compute_session_metrics',
-    'summarize_metrics',
-    'write_metrics_files',
-]
